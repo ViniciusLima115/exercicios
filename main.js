699fff9209b798d6410ce78320d769efbedc29a7
Vinicius Lima <83377988+ViniciusLima115@users.noreply.github.com>
2023-03-22 09:11:58 -0700

    exercicio_plugins_jquery

---
+++ main.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+
+    $('#telefone').mask('(00)00000-0000',{
+        placeholder: '(XX)XXXXX-XXXX'
+    })
+
+    $('#cpf').mask('00000000000',{
+        placeholder:'XXX.XXX.XXX-XX'
+    })
+
+    $('#cep').mask('00000000',{
+        placeholder:'XXXXX-XXX'
+    })
+
+    $('form').validate({
+        rules:{
+            nome:{
+                required:true
+            },
+            email:{
+                required:true,
+                email:true
+            },
+            telefone:{
+                required:true
+            },
+            cpf:{
+                required:true
+            },
+            enderecoCompleto:{
+                required:true
+            },
+            cep:{
+                required:true
+            }
+
+        },
+        messages:{
+            nome: 'Por Favor insira seu nome',
+            email: {
+                required: "Por favor, insira um endereço de e-mail.",
+                email: "Por favor, insira um endereço de e-mail válido."
+            },
+            telefone: "Por favor, insira um número de telefone válido.",
+            cpf: "Por favor, insira um CPF válido.",
+            enderecoCompleto: "Por favor, insira seu endereço completo.",
+            cep: "Por favor, insira um CEP válido."
+        },
+        submitHandler: function(form){
+            console.log(form)
+            form.reset();
+        },
+        invalidHandler: function(evento, validador){
+            let camposIncorretos = validador.numberOfInvalids();
+            if(camposIncorretos){
+                alert(`Existem ${camposIncorretos} Campos Incorretos`)
+            }
+        }
+    })
+
+})
